6d3ce5bb16fa62502abb2f4eacd520e575451b48
Luke Wren <wren6991@gmail.com>
2020-12-14 17:22:41 +0000

    Update sync_fifo: smaller and faster, support non-po2 sizes. Thanks tnt!

--- common/sync_fifo.v
+++ common/sync_fifo.v
@@ -2,7 +2,7 @@
  * DO WHAT THE FUCK YOU WANT TO AND DON'T BLAME US PUBLIC LICENSE     *
  *                    Version 3, April 2008                           *
  *                                                                    *
- * Copyright (C) 2018 Luke Wren                                       *
+ * Copyright (C) 2018-2020 Luke Wren                                  *
  *                                                                    *
  * Everyone is permitted to copy and distribute verbatim or modified  *
  * copies of this license document and accompanying software, and     *
@@ -16,63 +16,73 @@
  *********************************************************************/
 
 // Synchronous FIFO
-//
-// No first-word-fallthrough.
-// All outputs are registered, apart from read data, which is
-// combinationally decoded from internal register-based memory.
-// Depth must be power of 2, != 1
+// DEPTH can be any integer >= 1.
 
 module sync_fifo #(
 	parameter DEPTH = 2,
 	parameter WIDTH = 32,
-	parameter W_ADDR = $clog2(DEPTH)	// SHOULD BE LOCALPARAM but this triggers bug in xilinx tools with clog2
+	parameter W_LEVEL = $clog2(DEPTH + 1)
 ) (
 	input  wire clk,
 	input  wire rst_n,
 
-	input  wire [WIDTH-1:0] w_data,
-	input  wire             w_en,
-	output wire [WIDTH-1:0] r_data,
-	input  wire             r_en,
+	input  wire [WIDTH-1:0]   w_data,
+	input  wire               w_en,
+	output wire [WIDTH-1:0]   r_data,
+	input  wire               r_en,
 
-	output  reg             full,
-	output  reg             empty,
-	output  reg [W_ADDR:0]  level
+	output wire               full,
+	output wire               empty,
+	output reg  [W_LEVEL-1:0] level
 );
 
-reg [WIDTH-1:0] mem[0:DEPTH-1];
+reg [WIDTH-1:0] mem [0:DEPTH];
+reg [DEPTH-1:0] valid;
 
-reg [W_ADDR-1:0] w_ptr;
-reg [W_ADDR-1:0] r_ptr;
-assign r_data = mem[r_ptr];
+// ----------------------------------------------------------------------------
+// Control and datapath
 
 wire push = w_en && !full;
 wire pop = r_en && !empty;
 
 always @ (posedge clk or negedge rst_n) begin
 	if (!rst_n) begin
-		full <= 1'b0;
-		empty <= 1'b1;
-		level <= {(W_ADDR+1){1'b0}};
-		w_ptr <= {W_ADDR{1'b0}};
-		r_ptr <= {W_ADDR{1'b0}};
-	end else begin
-		w_ptr <= w_ptr + push;
-		r_ptr <= r_ptr + pop;
-		if (push) begin
-			mem[w_ptr] <= w_data;
-			if (!pop) begin
-				level <= level + 1'b1;
-				empty <= 1'b0;
-				full <= level == DEPTH - 1;
-			end
-		end else if (pop) begin
-			level <= level - 1'b1;
-			full <= 1'b0;
-			empty <= level == 1;
+		valid <= {DEPTH{1'b0}};
+	end else if (w_en || r_en) begin
+		// 2 LUTs 1 FF per flag, all FFs have same clke
+		valid <= (valid << push | {{DEPTH-1{1'b0}}, push}) >> pop;
+	end
+end
+
+// No reset on datapath
+always @ (posedge clk) begin: shift_data
+	integer i;
+	for (i = 0; i < DEPTH; i = i + 1) begin: data_stage
+		if (r_en || (w_en && !valid[i] && (i == DEPTH - 1 || !valid[i + 1]))) begin
+			mem[i] <= valid[i + 1] ? mem[i + 1] : w_data;
 		end
 	end
 end
+
+always @ (*) mem[DEPTH] = w_data;
+assign r_data = mem[0];
+
+// ----------------------------------------------------------------------------
+// Flags
+
+assign full = valid[DEPTH-1];
+assign empty = !valid[0];
+
+always @ (posedge clk or negedge rst_n) begin
+	if (!rst_n) begin
+		level <= {W_LEVEL{1'b0}};
+	end else begin
+		level <= level + push - pop;
+	end
+end
+
+// ----------------------------------------------------------------------------
+// Testbench junk vvv
 
 //synthesis translate_off
 always @ (posedge clk)
